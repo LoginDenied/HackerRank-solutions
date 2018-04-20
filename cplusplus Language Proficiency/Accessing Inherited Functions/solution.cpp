f03239c8ddbb333da3421d6e7e374721f0f82bf4
Adjovi Sim <adjovi1996@hotmail.com>
2018-04-20 19:02:07 +0800

    Fix error in spacing

--- cplusplus Language Proficiency/Accessing Inherited Functions/solution.cpp
+++ cplusplus Language Proficiency/Accessing Inherited Functions/solution.cpp
@@ -10,56 +10,56 @@ class D : public A, public B, public C //Require to add parent classes here for 
 		 }
 
 
-		 void update_val(int new_val)
-		 {
-             int targetValue;
-             targetValue = new_val;
+		void update_val(int new_val)
+		{
+			int targetValue;
+             		targetValue = new_val;
              
-             bool checkDivisibleTwo = true;
-             while (checkDivisibleTwo == true)
-             {
-                 if (targetValue % 2 == 0)
-                 {
-                     A::func(val);
-                     targetValue = targetValue / 2;
-                 }
-                 else
-                 {
-                     checkDivisibleTwo = false;
-                 }
-                 
-             }
+            		bool checkDivisibleTwo = true;
+             		while (checkDivisibleTwo == true)
+             		{
+                 		if (targetValue % 2 == 0)
+                 		{
+                     			A::func(val);
+					targetValue = targetValue / 2;
+				}
+				else
+				{
+					checkDivisibleTwo = false;
+				}
+
+			}
 
 			bool checkDivisibleThree = true;
-            while (checkDivisibleThree == true)
-             {
-                 if (targetValue % 3 == 0)
-                 {
-                     B::func(val);
-                     targetValue = targetValue / 3;
-                 }
-                 else
-                 {
-                     checkDivisibleThree = false;
-                 }
-                 
-             }
-             
-            bool checkDivisibleFive = true;
-            while (checkDivisibleFive == true)
-             {
-                 if (targetValue % 5 == 0)
-                 {
-                     C::func(val);
-                     targetValue = targetValue / 5;
-                 }
-                 else
-                 {
-                     checkDivisibleFive = false;
-                 }
-                 
-             }
-		 }
-		 //For Checking Purpose
-		 void check(int); //Do not delete this line.
+			while (checkDivisibleThree == true)
+			{
+				if (targetValue % 3 == 0)
+				{
+					B::func(val);
+					targetValue = targetValue / 3;
+				}
+				else
+				{
+					checkDivisibleThree = false;
+				}
+
+			}
+
+           		bool checkDivisibleFive = true;
+		   	while (checkDivisibleFive == true)
+		     	{
+				if (targetValue % 5 == 0)
+				{
+					C::func(val);
+					targetValue = targetValue / 5;
+			 	}
+			else
+			{
+				checkDivisibleFive = false;
+			}
+
+			}
+		}
+		//For Checking Purpose
+		void check(int); //Do not delete this line.
 };
